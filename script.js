69f8ab9bf8212b3bde3adb57b70684fc0054ae25
RafidKibria <62840938+RafidKibria@users.noreply.github.com>
2025-03-26 22:00:53 +0000

    addded auto lebron

--- script.js
+++ script.js
@@ -5,4 +5,16 @@ function feedLebron(){
     document.getElementById('lebron-pts').innerText = points;
 }
 
+let isLebronAuto = false;
+let counter = 0
 document.getElementById('lebron-clicker-image').addEventListener("click", feedLebron);
+document.getElementById('automatic-lebron-button').addEventListener("click", (e) =>
+    {counter += 1
+    setInterval(() =>{
+            points += 1 * counter
+            document.getElementById('lebron-pts').innerText = points;
+            }, 1000)})
+
+
+
+
